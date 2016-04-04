fed00ed8784976562310e92dde28bfb37c658dc9
Corey Maylone <Corey@livio.io>
2016-04-04 17:07:38 -0400

    Slice

--- models/tracker.js
+++ models/tracker.js
@@ -3,7 +3,7 @@ var Location = require('./location');
 var Tracker = function() {
     var self = this;
     self.currentLocations = new Map();
-    
+
     // Initilize current locations
     Location.all().forEach((location) => {
         self.currentLocations.set(location, []);
@@ -22,17 +22,17 @@ Tracker.prototype.current = function(cb) {
 Tracker.prototype.add = function(locationId, name, cb) {
     var self = this;
     Location.findById(locationId, function(err, location) {
-        if(err) {
+        if (err) {
             return cb(err);
-        } else if(!location) {
+        } else if (!location) {
             return cb(new Error('location with id ' + locationId + ' not found'));
         } else {
             self.findAndRemove(name, function(err) {
-                if(err) {
+                if (err) {
                     return cb(err);
                 } else {
                     console.log('Adding ' + name + ' to ' + location.name);
-                    self.currentLocations.get(location).push({name: name})
+                    self.currentLocations.get(location).push({ name: name })
                     return cb();
                 }
             })
@@ -43,17 +43,17 @@ Tracker.prototype.add = function(locationId, name, cb) {
 // Try and find user and remove them;
 Tracker.prototype.findAndRemove = function(name, cb) {
     var self = this;
-    
-    self.currentLocations.forEach((people, key) => {
-        people.forEach((person) => {
-            if(name === person.name) {
+
+    self.currentLocations.forEach((people, location) => {
+        people.forEach((person, index) => {
+            if (name === person.name) {
                 console.log('Removing ' + name + ' from ' + key);
-                self.currentLocations.delete(key);
+                self.currentLocations.set(location) = people.splice(index, 0);
                 return cb();
             }
         })
     })
-    
+
     return cb();
 }
 
