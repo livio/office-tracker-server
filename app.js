5689e417893251d2b85e36e5e569caf5748c6e51
Corey Maylone <Corey@livio.io>
2016-04-06 11:38:34 -0400

    clean up unused libs

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var locations = require('./routes/location');
@@ -13,20 +11,12 @@ var env = process.env.NODE_ENV || 'development';
 app.locals.ENV = env;
 app.locals.ENV_DEVELOPMENT = env == 'development';
 
-// view engine setup
-
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// app.use(favicon(__dirname + '/public/img/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', locations);
+app.use('/location', locations);
 
 module.exports = app;
--- routes/location.js
+++ routes/location.js
@@ -3,14 +3,14 @@ var router = express.Router();
 var tracker = require('../models/tracker');
 
 /* Get current location */
-router.get('/location', function(req, res) {
+router.get('/', function(req, res) {
   tracker.format(function(err, result) {
     res.send(result);
   })
 });
 
 /* Remove user from location */
-router.delete('/location', function(req, res, next) {
+router.delete('/', function(req, res, next) {
   if(!req.body.name) {
     return res.status(400).send({ error: 'name is not defined in body' })
   } else {
@@ -25,7 +25,7 @@ router.delete('/location', function(req, res, next) {
 })
 
 /* Add user to location */
-router.post('/location', function(req, res, next) {
+router.post('/', function(req, res, next) {
   if (!req.body.location) {
     return res.status(400).send({ error: 'location is not defined in body' })
   } else if (!req.body.name) {
